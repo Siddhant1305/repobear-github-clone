7373e5c7669c8061ba9d60bf08105a159ddb8f51
Siddhant Pandey <siddhantpandey1305@gmail.com>
2025-02-20 23:44:40 +0530

    issueFunctionality

---
+++ backend/controllers/issueController.js
@@ -0,0 +1,19 @@
+const createIssue = (req, res) => {
+    res.send("Issue Created!");
+};
+
+const updateIssueById = (req, res) => {
+    res.send("Issue Updated!");
+};
+
+const deleteIssueById = (req, res) => {
+    res.send("Issue Deleted!");
+};
+
+const getAllIssues = (req, res) => {
+    res.send("All Issues fetched!");
+};
+
+const getIssueById= (req, res) => {
+    res.send("Issues details fetched!");
+};
