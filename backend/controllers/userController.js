132050d71867f99512651b013d4d899fc2025ce7
Siddhant Pandey <siddhantpandey1305@gmail.com>
2025-02-21 17:39:12 +0530

    userSignup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,64 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const uri = process.env.MONGODB_URI;
+
+let client;
+
+async function connectClient() {
+    if (!client) {
+      client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      await client.connect();
+    }
+}
+
 const getAllUsers = (req, res) => {
     res.send("All users fetched!");
 };
 
-const signup = (req, res) => {
-    res.send("Signing Up!");
-};
+async function signup (req, res) {
+    const { username, password, email } = req.body;
+    try {
+        await connectClient();
+        const db = client.db("repobeargithubclone");
+        const usersCollection = db.collection("users");
+  
+        const user = await usersCollection.findOne({ username });
+        if (user) {
+            return res.status(400).json({ message: "User already exists!" });
+        }
+  
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+  
+        const newUser = {
+            username,
+            password: hashedPassword,
+            email,
+            repositories: [],
+            followedUsers: [],
+            starRepos: [],
+        };
+  
+        const result = await usersCollection.insertOne(newUser);
+  
+        const token = jwt.sign(
+            { id: result.insertId },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: "1h" }
+        );
+        res.json({ token, userId: result.insertId });
+    } catch (err) {
+        console.error("Error during signup : ", err.message);
+        res.status(500).send("Server error");
+    }
+}
 
 const login = (req, res) => {
     res.send("Logging In!");
--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const UserController = require("../controllers/userController.js");
+const userController = require("../controllers/userController");
 
 const userRouter = express.Router();
 
-userRouter.get("/allUsers", UserController.getAllUsers);
-userRouter.post("/signup", UserController.signup);
-userRouter.post("/login", UserController.login);
-userRouter.get("/userProfile", UserController.getUserProfile);
-userRouter.put("/updateProfile", UserController.updateUserProfile);
-userRouter.delete("/deleteProfile", UserController.deleteUserProfile);
+userRouter.get("/allUsers", userController.getAllUsers);
+userRouter.post("/signup", userController.signup);
+userRouter.post("/login", userController.login);
+userRouter.get("/userProfile/:id", userController.getUserProfile);
+userRouter.put("/updateProfile/:id", userController.updateUserProfile);
+userRouter.delete("/deleteProfile/:id", userController.deleteUserProfile);
 
 module.exports = userRouter;
