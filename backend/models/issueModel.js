20c522f2e8eec8bfc06e283741faadb22e97f836
Siddhant Pandey <siddhantpandey1305@gmail.com>
2025-02-27 23:18:10 +0530

    fetchRepo

--- backend/models/issueModel.js
+++ backend/models/issueModel.js
@@ -3,26 +3,27 @@ const { Schema } = mongoose;
 
 const IssueSchema = new Schema({
   title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["open", "closed"],
-    default: "open",
-  },
-  repository: {
-    type: Schema.Types.ObjectId,
-    ref: "Repository",
-    required: true,
-  },
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["open", "closed"],
+        default: "open",
+    },
+    repository: {
+        type: Schema.Types.ObjectId,
+        ref: "Repository",
+        required: true,
+    },
 }, {
-  timestamps: true,
+    timestamps: true,
 });
 
 const Issue = mongoose.model("Issue", IssueSchema);
+
 module.exports = Issue;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,37 +2,37 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-  },
-  repositories: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Repository",
+    username: {
+        type: String,
+        required: true,
+        unique: true,
     },
-  ],
-  followedUsers: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
-  ],
-  starRepos: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Repository",
+    password: {
+        type: String,
     },
-  ],
+    repositories: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: "Repository",
+        },
+    ],
+    followedUsers: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        },
+    ],
+    starRepos: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: "Repository",
+        },
+    ],
 }, {
   timestamps: true,
 });
